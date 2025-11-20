9fae93181159387ea7358002fdb2cb3580adfdb1
panhuida <panhuida@qq.com>
2025-11-20 21:23:32 +0800

    优化公众号文章页面布局

--- wechat-cli.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM 微信公众号文章助手 - 命令行工具 (Windows批处理文件)
-REM 使用方法:
-REM   wechat-cli download <article_url>
-REM   wechat-cli download --file <file_path>
-REM   wechat-cli download <article_url> --output <output_dir>
-
-python "%~dp0wechat-cli.py" %*
